0ad4943c9df79773287555484aea7220a074ad00
Alexander Aboutanos <alexanderaboutanos@gmail.com>
2021-11-07 16:24:06 -0500

    add initial files

---
+++ expressError.js
@@ -0,0 +1,10 @@
+/** @format */
+
+class ExpressError extends Error {
+  constructor(message, status) {
+    super();
+    this.message = message;
+    this.status = status;
+    console.error(this.stack);
+  }
+}
---
+++ fakeDb.js
@@ -0,0 +1,8 @@
+/** @format */
+
+global.items = [];
+
+module.exports = items;
+
+// Sample Entry
+// items = [{"name":"pickles", "price":10.99}]
